f538566705f831c5f5e9001413f4e5b1e1dbcf03
SteveRuble <SteveRuble@users.noreply.github.com>
2022-01-24 11:00:22 -0500

    Bump version to 0.0.5

--- formula/ih-core.rb
+++ formula/ih-core.rb
@@ -1,5 +1,5 @@
 class IhCore < Formula
-  VERSION="0.0.4"
+  VERSION="0.0.5"
   desc "Brew formula for installing core tools used at Included Health engineering."
   homepage "https://github.com/ConsultingMD/homebrew-ih-public"
   license "CC BY-NC-ND 4.0"
