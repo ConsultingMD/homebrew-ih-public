493e2e96e60f1f0107c87fbc55dacabc631f1eb1
SteveRuble <SteveRuble@@users.noreply.github.com>
2022-02-01 12:10:24 -0500

    fix some bugs found while setting up

--- lib/core/certificates/step.sh
+++ lib/core/certificates/step.sh
@@ -18,7 +18,6 @@ function ih::setup::core.certificate::help() {
         - Update the OpenSSL used by Homebrew
         - Tell node about the CA using NODE_EXTRA_CA_CERTS
         - Tell npm/yarn about CA certs
-        - If Java is installed, update Java keystore for bazel
     '
 }
 
@@ -31,11 +30,6 @@ function ih::setup::core.certificate::test() {
   fi
 
   if [ ! -d "$HOME/.ih/certs" ]; then
-    return 1
-  fi
-
-  if ! ih::setup::core.certificate::private::java-certs; then
-    ih::log::debug "DLP certs not installed in Java keystore"
     return 1
   fi
 
@@ -70,89 +64,10 @@ function ih::setup::core.certificate::install() {
   ih::log::info "Rehashing brew OpenSSL certs..."
   "$(brew --prefix)"/opt/openssl/bin/c_rehash
 
-  ih::setup::core.certificate::private::java-certs "install"
 
   ih::file::add-if-not-present "$HOME/.npmrc" "cafile=\"$CA_PATH\""
   ih::file::add-if-not-present "$HOME/.yarnrc" "cafile=\"$CA_PATH\""
 
   cp -f "$IH_CORE_LIB_DIR/core/certificates/default/11_certificates.sh" "$IH_DEFAULT_DIR/11_certificates.sh"
-
-}
-
-# If $1 is set to "install" then this installs the certificates in the java cacerts store.
-# Otherwise this returns 0 if the certificates are already installed, 1 if they are not.
-function ih::setup::core.certificate::private::java-certs() {
-  local CA_DIR="$HOME/.ih/certs"
-  local JAVA_HOME JAVA_EXISTS JAVA_CERT_DIR
-  JAVA_HOME=$(/usr/libexec/java_home)
-  JAVA_EXISTS=$?
-  if [[ $JAVA_EXISTS -ne 0 ]]; then
-    return 0
-  fi
-
-  for JAVA_CERT_DIR_CANDIDATE in "/lib/security/cacerts" "/jre/lib/security/cacerts"; do
-    JAVA_CERT_DIR="${JAVA_HOME}${JAVA_CERT_DIR_CANDIDATE}"
-    ih::log::debug "Looking for java install with certs at ${JAVA_CERT_DIR}..."
-
-    if [ -f "$JAVA_CERT_DIR" ]; then
-      ih::log::debug "Found java install with certs at $JAVA_CERT_DIR..."
-
-      ih::log::debug "Installing certs in store"
-
-      local -a CA_PATHS=(
-        "$CA_DIR/grand_rounds_dlp_ca.pem"
-        "$CA_DIR/grand_rounds_root_ca.pem"
-      )
-
-      for CA_PATH in "${CA_PATHS[@]}"; do
-        local FINGERPRINT FINGERPRINT_OK
-        FINGERPRINT=$(openssl x509 -in "$CA_PATH" -noout -fingerprint | cut -d"=" -f2)
-        FINGERPRINT_OK=$?
-        if [ ! $FINGERPRINT_OK ]; then
-          return 1
-        fi
-
-        if keytool -list -v -storepass changeit -keystore "$JAVA_CERT_DIR" | grep -q "$FINGERPRINT"; then
-          ih::log::debug "Cert store already contains GR CA cert from $CA_PATH"
-          continue
-        else
-          if [[ "$1" != "install" ]]; then
-            # Not installing, just checking, so we should return 1
-            # to indicate that the certs have not been installed.
-            ih::log::debug "Cert from path $CA_PATH not found in store"
-            return 1
-          fi
-        fi
-
-        ih::log::info "Adding cert $CA_PATH to $JAVA_CERT_DIR..."
-        local CA_NAME
-        CA_NAME=$(basename "$CA_PATH")
-
-        ih::log::info "Adding cert to Java-usable keystore (needs sudo access)"
-        sudo keytool -import -noprompt -storepass changeit -trustcacerts -alias "${CA_NAME%%.*}" -file "$CA_PATH" -keystore "$JAVA_CERT_DIR" || return 1
-      done
-
-      break
-    fi
-  done
-
-  # JAVA_CERT_DIR will be set to the store where the cert was installed
-
-  local BAZEL_RC="startup --host_jvm_args=\"-Djavax.net.ssl.trustStore=$JAVA_CERT_DIR\"
-startup --host_jvm_args=-Djavax.net.ssl.keyStorePassword=changeit"
-  if [[ "$1" == "install" ]]; then
-    touch "$HOME/.bazelrc"
-  fi
-  if ! grep -q "$JAVA_CERT_DIR" "$HOME/.bazelrc"; then
-    if [[ "$1" != "install" ]]; then
-      return 1
-    fi
-    ih::log::info "Configuring bazel to use custom trust store..."
-    if ! ih::file::add-if-not-matched "$HOME/.bazelrc" "startup" "$BAZEL_RC"; then
-      ih::log::error "Could not add startup command to $HOME/.bazelrc, bazel may not work correctly until you update it"
-      ih::log::error "Add these lines to $HOME/.bazelrc (or update the existing startup to set the trustStore):"
-      ih::log::error "$BAZEL_RC"
-    fi
-  fi
 
 }
--- lib/core/jira/default/09_jira.sh
+++ lib/core/jira/default/09_jira.sh
@@ -2,13 +2,15 @@
 
 # This script decrypts the JIRA creds file
 # and evals it to load the JIRA token
-
+set +xe
 if [ -z "$JIRA_USERNAME" ]; then
   # Back compat for people who already encrypted their JIRA creds
   if [ -f "$HOME/.jira/creds.gpg" ]; then
     eval gpg --use-agent --no-tty --quiet -o - "$HOME/.jira/creds.gpg"
-  else
-    export JIRA_API_TOKEN
-    JIRA_API_TOKEN=$(security find-generic-password -s "jira-api" -w "${TEST_KEYCHAIN:-default}")
   fi
+fi
+
+if [ -z "$JIRA_API_TOKEN" ]; then
+    JIRA_API_TOKEN=$(security find-generic-password -s "jira-api" -w)
+    export JIRA_API_TOKEN
 fi
