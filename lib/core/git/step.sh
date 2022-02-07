1b4fff0fa3b6cab24a7e631a1d37ffc5da39869a
SteveRuble <SteveRuble@@users.noreply.github.com>
2022-02-07 09:44:15 -0500

    added pre-commit and better docs to git step

--- lib/core/git/step.sh
+++ lib/core/git/step.sh
@@ -4,9 +4,12 @@ function ih::setup::core.git::help() {
   echo "Configure git settings
 
     This step will:
-    - update your global git config to use some good defaults
-    - create $GR_HOME if it doesn't exist
-    - create a default global .gitignore if one doesn't exist"
+    - Update your global git config to use some good defaults
+      This will not overwrite any existing settings that you have set.
+    - Create $GR_HOME if it doesn't exist.
+    - Create a default global .gitignore if one doesn't exist.
+    - Set up pre-commit to install automatically when repos are cloned."
+
 }
 
 function ih::setup::core.git::test() {
@@ -32,12 +35,25 @@ function ih::setup::core.git::install() {
   # Profile must be valid before we can setup git
   ih::setup::core.shell::private::configure-profile
 
-  git config --global user.name "${GITHUB_USER}"
-  git config --global user.email "${GITHUB_EMAIL_ADDRESS}"
-  git config --global color.ui true
-  git config --global core.excludesfile "${HOME}/.gitignore_global"
-  git config --global push.default simple
+  ih::setup::core.git::private::set-if-unset "user.name" "${GITHUB_USER}"
+  ih::setup::core.git::private::set-if-unset "user.email" "${GITHUB_EMAIL_ADDRESS}"
+  ih::setup::core.git::private::set-if-unset "color.ui" "true"
+  ih::setup::core.git::private::set-if-unset "core.excludesfile" "${HOME}/.gitignore_global"
+  ih::setup::core.git::private::set-if-unset "push.default" "simple"
+
+  # make git use ssh for everything
   git config --global url.ssh://git@github.com/.insteadOf https://github.com/
+
+  # set up pre-commit to automatically be set up for all cloned repos,
+  # if the user doesn't have a templateDir already
+  if git config --global init.templateDir; then
+    ih::log::warn "Detected existing templateDir, not setting up pre-commit auto-enable."
+  else
+    local GIT_TEMPLATE_DIR="${IH_DIR}/git-template"
+    mkdir "$GIT_TEMPLATE_DIR" 2>/dev/null || :
+    git config --global init.templateDir "$GIT_TEMPLATE_DIR"
+    pre-commit init-templatedir "$GIT_TEMPLATE_DIR"
+  fi
 
   # Make sure the desired src directory exists if GR_HOME is declared
   [[ -n ${GR_HOME} ]] && mkdir -p "${GR_HOME}"
@@ -45,9 +61,12 @@ function ih::setup::core.git::install() {
   # Copy the gitignore template into global if there isn't already a global.
   cp -n "${IH_CORE_LIB_DIR}/core/git/gitignore" "${HOME}/.gitignore_global" || :
 
-  echo "Updated git global config as follows:"
+  ih::log::info "Updated git global config as follows:"
   git -P config --global --list
   echo ""
+}
 
-  echo "Git configuration completed."
+# sets global config setting $1 to value $2 if it is not currently set
+function ih::setup::core.git::private::set-if-unset() {
+  git config --global "$1" || git config --global "$1" "$2"
 }
