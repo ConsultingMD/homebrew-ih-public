5e18b406237def0e947f8e724b09b5ab29f127de
SteveRuble <steve.ruble@users.noreply.github.com>
2022-04-22 14:48:32 -0400

    be more safe

--- lib/core/asdf/default/90_asdf.sh
+++ lib/core/asdf/default/90_asdf.sh
@@ -3,18 +3,23 @@
 
 # This adds the asdf shims to the shell.
 
-CURRENT_SHELL=$(ps -cp "$$" -o command="")
+function source_asdf() {
+  local CURRENT_SHELL
+  CURRENT_SHELL=$(ps -cp "$$" -o command="")
 
-if [ -f "$(brew --prefix asdf 2>/dev/null)/libexec/asdf.sh" ]; then
-  . "$(brew --prefix asdf)/libexec/asdf.sh"
-elif [ -f "$HOME/.asdf/asdf.sh" ]; then
-  . "$HOME/.asdf/asdf.sh"
-  # Source bash completions
-  if [[ $CURRENT_SHELL =~ "bash" ]]; then
-    . "$HOME/.asdf/completions/asdf.bash"
+  if [ -f "$(brew --prefix asdf 2>/dev/null)/libexec/asdf.sh" ]; then
+    . "$(brew --prefix asdf)/libexec/asdf.sh"
+  elif [ -f "$HOME/.asdf/asdf.sh" ]; then
+    . "$HOME/.asdf/asdf.sh"
+    # Source bash completions
+    if [[ $CURRENT_SHELL =~ "bash" ]]; then
+      . "$HOME/.asdf/completions/asdf.bash"
+    fi
   fi
-fi
 
-# Add completions to fpath for zsh
-# shellcheck disable=SC2206
-fpath=("${ASDF_DIR}/completions" $fpath)
+  # Add completions to fpath for zsh
+  # shellcheck disable=SC2206
+  fpath=("${ASDF_DIR}/completions" $fpath)
+}
+
+source_asdf
