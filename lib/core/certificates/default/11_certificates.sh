76941125b46aa3e7ed7f6d5797d30707b0393e8d
Steve Ruble <SteveRuble@users.noreply.github.com>
2022-09-26 11:56:51 -0400

    add deps and fix certs (#23)
    * [DP-1349] add yamllint to dependencies
    
    * [DP-1350] improve DLP certificate environment
    
    * [DP-1354] add wget dependency

--- lib/core/certificates/default/11_certificates.sh
+++ lib/core/certificates/default/11_certificates.sh
@@ -3,4 +3,14 @@
 # This script adds environment variables needed for
 # our DLP certificates to be respected
 
-export NODE_EXTRA_CA_CERTS="$HOME/.ih/certs/grand_rounds_chained_ca.pem"
+# Tell OpenSSL to use our cert bundle.
+export SSL_CERT_FILE="$HOME/.ih/certs/mozilla.pem"
+
+# Tell node and npm to use our cert bundle.
+export NODE_EXTRA_CA_CERTS="$SSL_CERT_FILE"
+
+# Tell Python requests library to use our cert bundle.
+export REQUESTS_CA_BUNDLE="$SSL_CERT_FILE"
+
+# Tell cURL to use our cert bundle.
+export CURL_CA_BUNDLE="$SSL_CERT_FILE"
