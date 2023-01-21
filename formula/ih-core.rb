653a5c04957980117eaaebb30332b6979f13c86f
Daniel Soto <120605455+DanielSotoEstrada@users.noreply.github.com>
2023-01-21 14:48:09 -0700

    Rancher Desktop added to the IH-SETUP (#28)
    * Rancher desktop added on ih-core.rb and added to the path on default/99
    
    * Bump version to 0.1.13

--- formula/ih-core.rb
+++ formula/ih-core.rb
@@ -1,5 +1,5 @@
 class IhCore < Formula
-  VERSION="0.1.12"
+  VERSION="0.1.13"
   desc "Brew formula for installing core tools used at Included Health engineering."
   homepage "https://github.com/ConsultingMD/homebrew-ih-public"
   license "CC BY-NC-ND 4.0"
@@ -20,6 +20,7 @@ class IhCore < Formula
   depends_on "coreutils"
   depends_on "yamllint"
   depends_on "wget"
+  depends_on "rancher"
 
   def install
     lib.install Dir["lib/*"]
