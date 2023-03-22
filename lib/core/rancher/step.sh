cdd7a4ca5b2f10c8ec64f17c2a729a3b243b5364
DanielSotoEstrada <dsoto@includedhealth.com>
2023-03-22 13:31:41 -0700

    DT-125 check if Rancher is Running and shut it down

--- lib/core/rancher/step.sh
+++ lib/core/rancher/step.sh
@@ -32,7 +32,9 @@ function ih::setup::core.rancher::install() {
 
     cp "${THIS_DIR}/io.rancherdesktop.profile.defaults.plist" "$HOME/Library/Preferences/io.rancherdesktop.profile.defaults.plist"
 
-
+    if command -v rdctl; then
+        $(rdctl shutdown)
+    fi
 
     CASKSUCCEEDED=1
     # Installation and configuration of Rancher Desktop
