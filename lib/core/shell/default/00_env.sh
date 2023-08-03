5fb009233a86e75d4b0339bc4a07dce9b3da418a
SteveRuble <SteveRuble@users.noreply.github.com>
2023-08-03 16:55:34 -0400

    fix: reduce impact of multiple brew archs
    If a user has x86 brew AND arm64 brew installed on an M1 they can get weird problems that are hard to understand because which is active at a given time can vary during the execution of ih-setup or other scripts. This change reduces the chances of different versions of brew becoming active at different times.

--- lib/core/shell/default/00_env.sh
+++ lib/core/shell/default/00_env.sh
@@ -15,8 +15,23 @@
 # Signals that IH shell augments have been sourced
 export IH_AUGMENT_SOURCED=yes
 
-# Activate brew paths
-[ -f "/usr/local/bin/brew" ] && eval "$(/usr/local/bin/brew shellenv)"
+# Prefer to activate the arm64 version of brew if it's available.
+# If it is, we don't want to activate the x86 version even if it's installed
+# because it will cause a lot of confusion. Really users shouldn't have
+# both installed at the same time, but we can't prevent that and
+# fixing it is a lot of work if you have packages installed across both.
+# Anyone who is actively using both versions on purpose is a power user
+# who can figure out how to switch between them.
+# For anyone who has both installed by accident we will try to minimize
+# the confusion.
+#
+# eval-ing the brew shellenv command puts the binaries installed by
+# that version of brew at the front of the PATH.
+if [ -f "/opt/homebrew/bin/brew" ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [ -f "/usr/local/bin/brew" ]; then
+  eval "$(/usr/local/bin/brew shellenv)"
+fi
 
 #Make sure home ~/bin is in the path
 [[ ! "$PATH" =~ ${HOME}/bin ]] && export PATH="${HOME}/bin:${PATH}"
