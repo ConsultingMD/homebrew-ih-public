f78e1a65cd2e1302a9a51e71ec91a6baa34582ee
Paul Brown <pbrown@doctorondemand.com>
2024-01-09 15:44:27 -0600

    update version in ih-rancher cask

--- Casks/ih-rancher.rb
+++ Casks/ih-rancher.rb
@@ -1,14 +1,14 @@
 cask "ih-rancher" do
-    version "1.8.1"
+    version "1.12.0"
 
-    if Hardware::CPU.intel? 
+    if Hardware::CPU.intel?
         url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v#{version}/Rancher.Desktop-#{version}.x86_64.dmg"
-        sha256 "5233a6b7024077bb2102f8c4afde7bed79207ab4d1989d8b2e34e5b353ef3d82"
+        sha256 "58388cf60514ca05fdc58bd5c8e2467f9fe5a3dec7371a31c1daeb22e4aeea17"
     else
         url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v#{version}/Rancher.Desktop-#{version}.aarch64.dmg"
-        sha256 "ed88c10760cf5ff03bfcfa68870a90bb839d79dd95a8357def11875fb1ffbd23"
+        sha256 "68464fd4520a09ba10ada6fc49bc20eeaee24e30931f52530be399cbec85e3b6"
     end
-    
+
     name "Rancher Desktop"
     desc "Alternative to Docker Desktop"
     homepage "https://rancherdesktop.io/"
