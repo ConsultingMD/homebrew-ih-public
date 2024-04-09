edb253a272e3f98333fd6ce44ab1829d94f06449
Paul Brown <67805647+pb-dod@users.noreply.github.com>
2024-04-09 11:52:11 -0500

    bump rancher 1.12.2 to 1.13.1 (fixes qemu on m3 mac) (#88)
    Release notes:
    https://github.com/rancher-sandbox/rancher-desktop/releases/tag/v1.13.1
    
    This also helps us get rid of some of the hacks we had to add to support
    m3 mac.

--- Casks/ih-rancher.rb
+++ Casks/ih-rancher.rb
@@ -1,12 +1,12 @@
 cask "ih-rancher" do
-    version "1.12.2"
+    version "1.13.1"
 
     if Hardware::CPU.intel?
         url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v#{version}/Rancher.Desktop-#{version}.x86_64.dmg"
-        sha256 "299034631ee0faa747f55b2ca7d6682419a15563a706ef8a2415e8638108fd35"
+        sha256 "75a5532f5d2332f07a556c75455367c26954471de5d4ba436f9ab4aa3b309ac2"
     else
         url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v#{version}/Rancher.Desktop-#{version}.aarch64.dmg"
-        sha256 "9ab8131239a34ff880b79617f71254a502fb30a3dd7908b13533b84e9698a499"
+        sha256 "7334e2246c8e0d015733697f9bc9abae18973296671595f6b9e039870d327388"
     end
 
     name "Rancher Desktop"
