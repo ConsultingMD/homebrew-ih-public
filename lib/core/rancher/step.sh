1ca5d3a0ee9f29e3524c06c5a0742cf91fe71110
Paul Brown <67805647+pb-dod@users.noreply.github.com>
2024-04-12 14:46:12 -0500

    CLOUD-3175: prevent parse error on macos version check (#93)
    @cobbr2 noticed this error while he was testing the recent m2/m3 mac
    changes:
    ```
    ln: /usr/local/bin/docker: File exists
    ln: /usr/local/bin/docker-compose: File exists
    
    Parse error: bad token
        <stdin>:1
    
    Rancher Desktop has been installed successfully
    ```
    
    This is caused by an undefined variable, so this PR fixes that.
    
    Rick also suggested I rewrite the version checking logic to avoid using
    `bc -l` to make it simpler.

--- lib/core/rancher/step.sh
+++ lib/core/rancher/step.sh
@@ -12,6 +12,8 @@ RANCHER_AUGMENT_DST="$IH_DEFAULT_DIR/11_rancher.sh"
 PLIST_SRC="$IH_CORE_LIB_DIR/core/rancher/io.rancherdesktop.profile.defaults.plist"
 PLIST_DST="$HOME/Library/Preferences/io.rancherdesktop.profile.defaults.plist"
 
+REQUIRED_APPLE_SILICON_MACOS_VERSION="13.3"
+
 # Check if the step has been installed and return 0 if it has.
 # Otherwise return 1.
 function ih::setup::core.rancher::test() {
@@ -51,10 +53,8 @@ function ih::setup::core.rancher::test() {
 
   # Use vz (requires macOS >=13.3) instead of qemu on M3 macs to resolve issues.
   # More details: https://github.com/lima-vm/lima/issues/1996
-  local macos_version=$(ih::arch::get_macos_version)
   if ih::arch::is_m2_or_m3_mac; then
-    if (( $(echo "$macos_version < 13.3" | bc -l) )); then
-      ih::log::error "macOS version 13.3 or higher is required for M3 Macs."
+    if ! ih::arch::check_macos_version_compatibility "$REQUIRED_APPLE_SILICON_MACOS_VERSION"; then
       return 1
     elif ! grep -q "<string>vz</string>" "$PLIST_DST"; then
       ih::log::debug "The PLIST file needs to be updated to use 'vz' for M3 Macs."
@@ -163,8 +163,7 @@ function ih::setup::core.rancher::install() {
     # Use vz (requires macOS >=13.3) instead of qemu on M3 macs to resolve issues.
     # More details: https://github.com/lima-vm/lima/issues/1996
     if ih::arch::is_m2_or_m3_mac; then
-      if (( $(echo "$macos_version < 13.3" | bc -l) )); then
-        ih::log::error "macOS version 13.3 or higher is required for M3 Macs."
+      if ! ih::arch::check_macos_version_compatibility "$REQUIRED_APPLE_SILICON_MACOS_VERSION"; then
         return 1 # Abort the installation for M3 Macs
       elif ! grep -q "<string>vz</string>" "$PLIST_DST"; then
         ih::log::debug "Updating PLIST to use 'vz' for Virtualization for M3 Macs."
