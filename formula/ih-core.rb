eb56b66a0dc173db4b7b7efe05e55648f5d4e022
Paul Brown <67805647+pb-dod@users.noreply.github.com>
2024-07-24 12:19:00 -0500

    prevent always detecting out-of-sync rancher plist file (#99)
    @JDuskey and @rachelkathleen were seeing the rancher step always saying
    "The PLIST file is out of sync":
    ```
    DBUG: File /Users/<redacted>/Library/Preferences/io.rancherdesktop.profile.defaults.plist does not match source
    DBUG: The PLIST file is out of sync.
    ```
    
    This is happening because the install step makes changes to the PLIST
    file, but the test step is comparing based on the original file - so it
    never takes those changes into account.
    
    This PR creates a temporary file, modifies it, and uses it for both the
    test and install steps.

--- formula/ih-core.rb
+++ formula/ih-core.rb
@@ -1,5 +1,5 @@
 class IhCore < Formula
-  VERSION="0.1.58"
+  VERSION="0.1.59"
   desc "Brew formula for installing core tools used at Included Health engineering."
   homepage "https://github.com/ConsultingMD/homebrew-ih-public"
   license "CC BY-NC-ND 4.0"
