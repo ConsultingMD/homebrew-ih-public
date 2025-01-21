fafc54bcdfbdb94f806744a87952647318a8763b
pb-dod <paul.brown@includedhealth.com>
2025-01-21 14:30:45 -0600

    Bump version to 0.1.70

--- formula/ih-core.rb
+++ formula/ih-core.rb
@@ -1,5 +1,5 @@
 class IhCore < Formula
-  VERSION="0.1.69"
+  VERSION="0.1.70"
   desc "Brew formula for installing core tools used at Included Health engineering."
   homepage "https://github.com/ConsultingMD/homebrew-ih-public"
   license "CC BY-NC-ND 4.0"
