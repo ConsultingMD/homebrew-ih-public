2118f6ee8fd42b828997a9a5bef62095bc4f51d9
pb-dod <paul.brown@includedhealth.com>
2025-01-21 14:28:00 -0600

    bump rancher 1.14.2 -> 1.16.0

--- Casks/ih-rancher.rb
+++ Casks/ih-rancher.rb
@@ -1,12 +1,12 @@
 cask "ih-rancher" do
-    version "1.14.2"
+    version "1.16.0"
 
     if Hardware::CPU.intel?
         url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v#{version}/Rancher.Desktop-#{version}.x86_64.dmg"
-        sha256 "e764e335d1475f8bceb3fb6d1d1892b09d3c2bee3f34355ddab8a7e157c87452"
+        sha256 "c63599d038ffd292e5d49c0f41ed520c0ef1a86dec66a1735d47e9aa9e533b20"
     else
         url "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v#{version}/Rancher.Desktop-#{version}.aarch64.dmg"
-        sha256 "fccf84337e0a894cda047ea1c5796e4de843a9f992665d28a9451e1097061bfc"
+        sha256 "9b1d56a5d606751edba48022a454f891e792f7aa36c38e681271b2104f59f7f4"
     end
 
     name "Rancher Desktop"
